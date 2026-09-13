5179992cfce0dc75d5e01963157d957866a2f22e
binaryjack <pianatadeo@hotmail.com>
2026-09-13 09:25:08 +0200

    chore: purge temporary fix scripts from codernic-ui

--- apps/codernic-ui/test_format.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Tadeop. All rights reserved.
-// Proprietary and Confidential Source Code.
-// Unauthorized copying, reproduction, or distribution of this file, via any medium,
-// is strictly prohibited under Non-Disclosure Agreement (NDA) and applicable law.
-
-import { getSchemaFormat } from '@atomos-web/structura';
-console.log(getSchemaFormat('appSettings', 'TSinMD'));
